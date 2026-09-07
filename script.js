2e582e291782ec0cc70851f0e9c7a20520bc1788
kgkhs1992-bld <kgkhs1992@gmail.com>
2026-09-07 23:59:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -176,7 +176,7 @@ if (!/^471CA\d{2,3}$/.test(pin)) {
         .select("*")
         .eq("roll_no", Number(rollNo))
         .eq("pin", pin)
-        .eq("assessment", assessment)
+        .in("assessment", [assessment, assessmentLabel])
         .maybeSingle();
 
 
