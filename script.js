ae3040a3093b6b638efbe37ea315e7f51af74b6a
kgkhs1992-bld <kgkhs1992@gmail.com>
2026-09-11 15:53:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -774,7 +774,7 @@ const RESULT_ASSESSMENTS = {
     ["UT2", "Unit Test 2", "VIII UT-2"],
     ["UT3", "Unit Test 3", "VIII UT-3"],
     ["UT4", "Unit Test 4", "VIII UT-4"]
-    }
+    ],
 
   IX: [
     ["FA1", "Formative Assessment 1", "IX FA-1"],
